dda3750c13416e172c538b62f1a338d9583dadca
diyorbek0309 <diyorbekolimov2000@gmail.com>
2021-10-30 11:23:44 +0500

    finished helper functions

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const handleSubmit = () => {
   const password = document.getElementById("password").value;
   const r_password = document.getElementById("r_password").value;
   const errors = document.getElementsByClassName("error");
+  if (!checkNames(firstName)) {
+    errors[0].innerHTML = "zfjksgajfgvjashfgvfv";
+  }
 };
 
 const checkNames = (name) => {
@@ -32,9 +35,45 @@ const checkUserName = (username) => {
   return result;
 };
 
-const checkPasswords = (p, r_p) => {
-  if (p.length >= 8) {
+const checkEmail = (email) => {
+  let regexEmail = /^\w+([\.-]?\ w+)*@\w+([\.-]?\ w+)*(\.\w{2,3})+$/;
+  if (email.match(regexEmail)) {
+    return true;
   }
+
+  return false;
 };
 
-console.log(checkUserName("diyorbek0309"));
+const checkPasswords = (p) => {
+  let chars = "@#$%^&",
+    l_letters = "abcdefghijklmnopqrstuvwxyz",
+    u_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers = "0123456789";
+  let hasLLetter = false,
+    hasULetter = false,
+    hasNumber = false,
+    hasChar = false;
+  if (p.length >= 8) {
+    for (let i = 0; i < p.length; i++) {
+      if (l_letters.includes(p[i])) {
+        hasLLetter = true;
+        continue;
+      } else if (u_letters.includes(p[i])) {
+        hasULetter = true;
+        continue;
+      } else if (numbers.includes(p[i])) {
+        hasNumber = true;
+        continue;
+      } else if (chars.includes(p[i])) {
+        hasChar = true;
+        continue;
+      }
+    }
+  }
+
+  return hasLLetter && hasULetter && hasNumber && hasChar;
+};
+
+const comparePasswords = (p, r_p) => p === r_p;
+
+console.log(checkEmail("diyorbe5454k@aaa.ru"));
